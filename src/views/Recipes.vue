75ce2c3ad85a6555b43f35d43fa18a3045f6a789
BiancaSeleveschi <bianca.seleveschi@gmail.com>
2023-02-23 10:50:06 +0200

    add search method to Recipes

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -2,10 +2,23 @@
   <div>
     <h1 class="fw-bold">Our Menu</h1>
     <div class="border border-bottom w-25 m-auto mt-3"></div>
+    <h4 class="m-3">Search recipe</h4>
+    <div class="m-auto">
+      <input
+        v-model="recipeTitle"
+        class="p-1"
+        type="text"
+        placeholder="Search"
+      />
+      <button class="p-1" id="search-button" @click="search">Search</button>
+      <div v-show="showSearchedRecipe">
+        <ItemList :recipes="searchedRecipe" />
+      </div>
+    </div>
     <div
       v-for="(category, index) in categories"
       :key="index"
-      class="d-inline-block m-3"
+      class="d-inline-block m-5"
     >
       <CategoryButtons
         :category="category"
@@ -35,20 +48,35 @@ export default {
       indexButtonCategory: -1,
       showRecipe: true,
       recipes: this.$store.state.recipes,
+      recipeTitle: "",
+      recipeTitle2: "",
+      showSearchedRecipe: false,
     };
   },
   computed: {
     categories() {
       return this.$store.getters.getAllCategories();
     },
+    searchedRecipe() {
+      return this.recipes.filter((recipe) =>
+        recipe.title.includes(this.recipeTitle)
+      );
+    },
   },
   methods: {
     filterByCategory(category) {
-      this.showRecipe = false;
       return this.$store.getters.getRecipesByCategory(category);
+    },
+    search() {
+      this.showSearchedRecipe = true;
+      this.showRecipe = false;
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+#search-button {
+  border-radius: 7px;
+}
+</style>
